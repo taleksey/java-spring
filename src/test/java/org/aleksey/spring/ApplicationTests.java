72af2e4c741b93508253f2de9599f58445dd0297
taleksey <alterehov@gmail.com>
2024-10-27 13:23:57 +0200

    Create main

---
+++ src/test/java/org/aleksey/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aleksey.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
